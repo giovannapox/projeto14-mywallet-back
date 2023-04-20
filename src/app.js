625c75655a2d94b64e025034ee6ab31d0a3e3872
Giovanna <giovannad1clonius@gmail.com>
2023-04-20 15:14:46 -0300

    Post semi-completo

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import { MongoClient } from "mongodb";
+import bcrypt from "bcrypt";
 
 // Criando o servidor
 const app = express();
@@ -20,6 +21,43 @@ try {
     console.log(err.message);
 }
 const db = mongoClient.db();
+
+// endpoints
+app.post("/cadastro", async (req, res) => {
+    const { nome, email, senha } = req.body;
+    const hash = bcrypt.hashSync(senha, 10);
+
+    try{
+        await db.collection('usuarios').insertOne({ nome, email, senha: hash });
+        res.sendStatus(201)
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
+});
+
+app.post("/", async (req, res) => {
+    try{
+
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
+});
+
+app.post("/nova-transacao/:tipo", async (req, res) => {
+    try{
+
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
+});
+
+app.get("/home", async (req, res) => {
+    try{
+
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
+});
 
 // App esperando requisições
 const PORT = 5000;
